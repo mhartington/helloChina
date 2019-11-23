f745456212029db9c1d9e96fcc286cbebf466793
Mike Hartington <mikehartington@gmail.com>
2019-11-22 20:53:17 -0500

    updates

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -14,6 +14,11 @@ const routes: Routes = [
             path: '',
             loadChildren: () =>
               import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          },
+          {
+            path: 'details',
+            loadChildren: () =>
+              import('../details/details.module').then(m => m.DetailsPageModule)
           }
         ]
       },
